c8171818dd54e565c488d1ac14a31653f2ddc5b9
rishab7711 <90062383+rishab7711@users.noreply.github.com>
2022-03-30 10:58:38 +0530

    Add files via upload

---
+++ Certificate.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=div.conatiner, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="\CERTIFICATE\CSS\Edit.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<div class="box">
+
+
+<div class="row" > <col-2 > Sr No. </col></div>
+<!-- <div id="barcode" style='float:right;'>
+  insert your custom barcode setting your data in the GET parameter "data"
+  <!-- <img alt='Barcode Generator TEC-IT'
+       src='https://barcode.tec-it.com/barcode.ashx?data=ABC-abc-1234&code=Code128&translate-esc=on'/>
+</div> --> 
+
+<div class="text-center">
+    
+    
+<span class="border border-danger" ><strong>SINCE 1996 </strong></span>
+
+
+
+<!-- tOP LOGO starts -->
+<div class="table-responsive">
+<table class="table table-borderless">
+
+<tr>
+
+<td  style="width:20%" ><img src="\CERTIFICATE\CSS\IMAGES\logo.png" class="img-fluid" alt="..."></td>
+    <td style="width:60%"> <h3 style="color:red;">SARAIDHELA CHAMBER OF COMMERCE</h3> 
+    <strong> <p style="color:red;"> Regn No.-26/2017-18(Under society act, jharkhand)</p> </strong>
+    </td>
+    <td style="width:20%"><img src="\CERTIFICATE\CSS\IMAGES\logo.png" class="img-fluid" alt="..."></td>
+
+</tr>
+
+</table>
+</div>
+<!-- TOP LOGO ends -->
+
+<!-- // middle -->
+
+   <div class="mid"> 
+  <div>  <p style="color:red;">Affiliated to the federation of dhanbad zilla chamber of commerce </p> </div>
+
+    <div class="text-center">off.: Amba Tower, Steel Gate, Saraidhela, Dhanbad-818127, E-mail : chambersaraidhella@gmail.com</div>
+
+    <div class="text-center"> <h2> <ul>MEMBER CERTIFICATE</ul>  </h2></div>
+
+    <div class="text-center"><p> <em>This is to certify that</em> </p></div>
+
+    </div>
+
+    </div>
+<!-- text center ends -->
+    <!-- middle 2 -->
+
+
+<div class="row">
+
+<tr><td><p><em>M/s.</em> </p></td></tr>
+
+<tr><td><p> <em>Address. </em></p></td></tr>
+<tr><td><p><em>is the honorable member of saraidhela chamber of commerce for the session.</em> </p></td></tr>
+</div>
+
+
+
+
+
+
+<!-- end begins -->
+
+<div class="footer">
+
+
+    <div class="box1"> <p> Shivashish Pandey</p>
+                      <p> President </p>  
+                        <p>  (9304476070) </p> </div>
+
+
+
+    <div class="box2"> <p> Ashutosh Kr. Ojha </p>
+    <p> Secretary </p>
+    <p> (9835929746) </p>  </div>
+
+
+
+    <div class="box3"><p>K.C. Prasad</p> 
+    <p>Treasurer</p>
+    <p>(9835145670)</p></div>
+</div>
+
+<!-- end ends -->
+
+</div>
+
+
+   </body>
+  </html>
